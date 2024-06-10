79ff06b44a8fa5a8943c0fd7b9a5ea4b806f6232
Ethan Jones <339467243@GoTVDSB.ca>
2024-06-10 13:20:09 -0400

    june 10

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace Looking_Back
 {
@@ -16,10 +19,20 @@ namespace Looking_Back
             Main,
             Death
         }
+        Random generator = new Random();
         Screen screen;
         MouseState mouseState;
         KeyboardState keyboardState;
-        Rectangle window, start;
+        Rectangle window, start, whiteMan, playerHealthBar;
+        Vector2 playerSpeed, snakeSpeed;
+        SoundEffect windOne, windTwo, windThree, windFour;
+        SoundEffectInstance windOneInstance, windTwoInstance, windThreeInstance, windFourInstance;
+        float snakeattackSpeed;
+        int walkAnimation, windNum = 1;
+        List <int> enemyHealth = new List<int>();
+        List <Rectangle> enemies = new List<Rectangle>();
+        List<Rectangle> enemiesHealthBar = new List<Rectangle>();
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -31,6 +44,9 @@ namespace Looking_Back
         {
             // TODO: Add your initialization logic here
             screen = Screen.Start;
+            enemyHealth.Clear();
+            window = new Rectangle(0, 0, 800, 600);
+            start = new Rectangle(350, 400, 100, 50);
             _graphics.ApplyChanges();
             base.Initialize();
         }
@@ -38,7 +54,14 @@ namespace Looking_Back
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+            windOne = Content.Load<SoundEffect>("Wind (1)");
+            windTwo = Content.Load<SoundEffect>("Wind (2)");
+            windThree = Content.Load<SoundEffect>("Wind (3)");
+            windFour = Content.Load<SoundEffect>("Wind (4)");
+            windOneInstance = windOne.CreateInstance();
+            windTwoInstance = windTwo.CreateInstance();
+            windThreeInstance = windThree.CreateInstance();
+            windFourInstance = windFour.CreateInstance();
             // TODO: use this.Content to load your game content here
         }
 
@@ -46,7 +69,38 @@ namespace Looking_Back
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
-            if ()
+            if (screen == Screen.Start)
+            {
+                if (windFourInstance.State != SoundState.Playing && windNum == 1)
+                {
+                    windOne.Play();
+                    windNum = 2;
+                }
+                else if (windOneInstance.State != SoundState.Playing && windNum == 2)
+                {
+                    windTwo.Play();
+                    windNum = 3;
+                }
+                 else if (windTwoInstance.State != SoundState.Playing && windNum == 3)
+                {
+                    windThree.Play();
+                    windNum = 4;
+                }
+                else if (windThreeInstance.State != SoundState.Playing && windNum == 4)
+                {
+                    windFour.Play();
+                    windNum = 1;
+                }
+            }
+            if (screen == Screen.Main)
+            {
+
+            }
+            if (screen == Screen.Animation)
+            {
+
+            }
+            if (screen == Screen.Death)
             {
 
             }
@@ -60,7 +114,24 @@ namespace Looking_Back
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            if (screen == Screen.Start)
+            {
+                //_spriteBatch.Draw(window, Color.AliceBlue);
+            }
+            if (screen == Screen.Main)
+            {
 
+            }
+            if (screen == Screen.Animation)
+            {
+
+            }
+            if (screen == Screen.Death)
+            {
+
+            }
+            _spriteBatch.End();
             base.Draw(gameTime);
         }
     }
