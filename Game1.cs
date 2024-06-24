486aff75c70ebddc8017749b03d824fc16050e1a
IloveMondays12 <ejrj2014@gmail.com>
2024-06-24 13:14:02 -0400

    june 24  115

--- Game1.cs
+++ Game1.cs
@@ -6,8 +6,6 @@ using Microsoft.Xna.Framework.Media;
 using System;
 using System.Collections.Generic;
 //using System.Drawing;
-//using System.Collections.Generic;
-//using System.Drawing;
 //Was saying it was ambiguous because this and microsoft xna had same shit?
 
 namespace Looking_Back
@@ -40,25 +38,27 @@ namespace Looking_Back
         walkingRightThree, stageOneBGone, stageOneBGtwo, jumpingRight, jumpingLeft, wButtonText, tarpText,
         bridgeText, crateText, goblinText, dartText;
        
-        SoundEffect windOne, windTwo, windThree, windFour, birds;
+        SoundEffect windOne, windTwo, windThree, windFour, birds, doorOpen, sickInk, goblinShoot;
 
         Song introSong;
 
-        SoundEffectInstance windOneInstance, windTwoInstance, windThreeInstance, windFourInstance;
+        SoundEffectInstance windOneInstance, windTwoInstance, windThreeInstance, windFourInstance, doorOpenInstance, sickInkInstance, goblinShootInstance;
 
         String stageOneBGstate;
 
         float snakeattackSpeed, gravitationalAcceleration = 0;
 
         int walkAnimation = 0, windNum = 1, introFade = 0, seconds = 0, gameTimer = 0, walkingAnimationStep = 2,
-        groundLevel = 590, previousGroundLevel = 590;
+        groundLevel = 590, previousGroundLevel = 590, goblinStartTime = 0, dartsX;
 
-        bool startAnimation, cobraAnimation, waterAnimation, walking, fade, ptIntro = false, wButton = false;
+        bool startAnimation, cobraAnimation, waterAnimation, walking, fade, ptIntro = false, wButton = false,
+            goblinActive = false;
 
         //List <int> enemyHealth = new List<int>();
         //List <Rectangle> enemies = new List<Rectangle>();
         //List<Rectangle> enemiesHealthBar = new List<Rectangle>();
         List<Rectangle> darts = new List<Rectangle>();
+        List<int> dartStartTimes = new List<int>();
 
         public Game1()
         {
@@ -79,7 +79,13 @@ namespace Looking_Back
             tarpRect = new Rectangle(800,400,250,200);
             bridgeRect = new Rectangle(1090,230,175,350);
             crateRect = new Rectangle(1050,490,90,90);
+            goblinRect = new Rectangle((bridgeRect.Right - 20), 530, 60, 60);
+            dartsX = goblinRect.Right;
             playerSpeed = new Vector2(3 , 13);
+            dartStartTimes.Add(generator.Next(30, 300));
+            dartStartTimes.Add(generator.Next(30, 300));
+            dartStartTimes.Add(generator.Next(30, 300));
+            dartStartTimes.Sort();
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.ApplyChanges();
@@ -96,6 +102,9 @@ namespace Looking_Back
             windThree = Content.Load<SoundEffect>("Wind (3)");
             windFour = Content.Load<SoundEffect>("Wind (4)");
             birds = Content.Load<SoundEffect>("Birds");
+            doorOpen = Content.Load<SoundEffect>("Door open");
+            sickInk = Content.Load<SoundEffect>("nice ink (lewis)");
+            goblinShoot = Content.Load<SoundEffect>("Dart goblin 2");
             introSong = Content.Load<Song>("Bill Withers - Ain't No Sunshine");
             introScreenText = Content.Load<Texture2D>("Intro screen");
             startBtnText = Content.Load<Texture2D>("Start button");
@@ -123,6 +132,9 @@ namespace Looking_Back
             windTwoInstance = windTwo.CreateInstance();
             windThreeInstance = windThree.CreateInstance();
             windFourInstance = windFour.CreateInstance();
+            goblinShootInstance = goblinShoot.CreateInstance();
+            doorOpenInstance = doorOpen.CreateInstance();
+            sickInkInstance = sickInk.CreateInstance();
             // TODO: use this.Content to load your game content here
         }
 
@@ -200,6 +212,10 @@ namespace Looking_Back
                 }
                 if (ptIntro == false)
                 {
+                    if (darts.Count != 0)
+                    {
+                        dartsX = dartsX + 4;
+                    }
                     if (keyboardState.IsKeyDown(Keys.D))
                     {
                         if (playerSpeed.X > 0)
@@ -234,13 +250,16 @@ namespace Looking_Back
                     }
                         if ((whiteManRect.X - window.X) >= 360 && (whiteManRect.X - (window.X + 3930)) <= -400 && (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.A)))
                         {
+                        
                             window.X = (window.X + (int)playerSpeed.X);
-                        wButtonRect.X = (wButtonRect.X + (int)playerSpeed.X);
-                        tarpRect.X = (tarpRect.X + (int)playerSpeed.X);
-                        crateRect.X = (crateRect.X + (int)playerSpeed.X);
-                        bridgeRect.X = (bridgeRect.X + (int)playerSpeed.X);
+                            wButtonRect.X = (wButtonRect.X + (int)playerSpeed.X);
+                            tarpRect.X = (tarpRect.X + (int)playerSpeed.X);
+                            crateRect.X = (crateRect.X + (int)playerSpeed.X);
+                            bridgeRect.X = (bridgeRect.X + (int)playerSpeed.X);
+                            goblinRect.X = (goblinRect.X + (int)playerSpeed.X);
+                        
 
-                    }
+                        }
                         else if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.A))
                         {
                             whiteManRect.X = whiteManRect.X - (int)playerSpeed.X;
@@ -261,6 +280,7 @@ namespace Looking_Back
                         tarpRect.X = (tarpRect.X - (int)playerSpeed.X);
                         crateRect.X = (crateRect.X - (int)playerSpeed.X);
                         bridgeRect.X = (bridgeRect.X - (int)playerSpeed.X);
+                        goblinRect.X = (goblinRect.X - (int)playerSpeed.X);
                     }
                     if (whiteManRect.Right <= (bridgeRect.Left + 100) && playerSpeed.X < 0 && whiteManRect.Right > (bridgeRect.Left + 95) && whiteManRect.Bottom > (bridgeRect.Top + 75))
                     {
@@ -269,6 +289,7 @@ namespace Looking_Back
                         tarpRect.X = (tarpRect.X - (int)playerSpeed.X);
                         crateRect.X = (crateRect.X - (int)playerSpeed.X);
                         bridgeRect.X = (bridgeRect.X - (int)playerSpeed.X);
+                        goblinRect.X = (goblinRect.X - (int)playerSpeed.X);
                     }
                     if (whiteManRect.Left >= (bridgeRect.Left + 95) && playerSpeed.X > 0 && whiteManRect.Left <= (bridgeRect.Left + 97) && whiteManRect.Bottom > (bridgeRect.Top + 75))
                     {
@@ -277,6 +298,16 @@ namespace Looking_Back
                         tarpRect.X = (tarpRect.X - (int)playerSpeed.X);
                         crateRect.X = (crateRect.X - (int)playerSpeed.X);
                         bridgeRect.X = (bridgeRect.X - (int)playerSpeed.X);
+                        goblinRect.X = (goblinRect.X - (int)playerSpeed.X);
+                    }
+                    if ((whiteManRect.Left + 50) >= bridgeRect.Right && whiteManRect.Bottom > 486 && (whiteManRect.X + 50) <= (bridgeRect.Right + 3))
+                    {
+                        window.X = (window.X - (int)playerSpeed.X);
+                        wButtonRect.X = (wButtonRect.X - (int)playerSpeed.X);
+                        tarpRect.X = (tarpRect.X - (int)playerSpeed.X);
+                        crateRect.X = (crateRect.X - (int)playerSpeed.X);
+                        bridgeRect.X = (bridgeRect.X - (int)playerSpeed.X);
+                        goblinRect.X = (goblinRect.X - (int)playerSpeed.X);
                     }
                     if (whiteManRect.Top <= (bridgeRect.Top + 75 ) && whiteManRect.Bottom >= (bridgeRect.Top + 75) && playerSpeed.Y>0 && whiteManRect.Right >= bridgeRect.Left && whiteManRect.Right <= (bridgeRect.Left + 115))
                     {
@@ -329,6 +360,24 @@ namespace Looking_Back
                     {
                         whiteManRect.Y = groundLevel - 120;
                         playerSpeed.Y = 13;
+                    }
+                    if ((whiteManRect.X - goblinRect.X) >= 80 && goblinActive == false)
+                    {
+                        doorOpenInstance.Play();
+                        goblinActive = true;
+
+                    }
+                    if (doorOpenInstance.State == SoundState.Stopped && goblinActive == true && goblinStartTime == 0)
+                    {
+                        goblinStartTime = gameTimer;
+                    }
+                    if (goblinActive == true && doorOpenInstance.State == SoundState.Stopped)
+                    {
+                        
+                        if (dartStartTimes.Contains(gameTimer - goblinStartTime))
+                        {
+                            darts.Add(new Rectangle((dartsX - ((dartStartTimes[0] - dartStartTimes[darts.Count])*4)), 560, 15, 10));
+                        }
                     }
                     
                     
@@ -465,8 +514,19 @@ namespace Looking_Back
                             _spriteBatch.Draw(jumpingLeft, whiteManRect, Color.White);
                         }
                     }
+                    if (goblinActive == true)
+                    {
+                        _spriteBatch.Draw(goblinText, goblinRect, Color.White);
+                    }
                     _spriteBatch.Draw(crateText, crateRect, Color.White);
                     _spriteBatch.Draw(bridgeText, bridgeRect, Color.White);
+                    if (darts.Count != 0)
+                    {
+                        for (int i = 0; i < darts.Count; i++)
+                        {
+                            _spriteBatch.Draw(dartText, darts[i], Color.White);
+                        }
+                    }
                 }
             }
 
